e682156fbd6e21a5bce88f5c405a59a22b4e448b
darkovrbaski <vrbaski.ra200.2018@uns.ac.rs>
2024-01-15 03:35:40 +0100

    add kubernetes

--- branch-library/src/main/java/me/darkovrbaski/branchlibrary/config/HttpClientConfig.java
+++ branch-library/src/main/java/me/darkovrbaski/branchlibrary/config/HttpClientConfig.java
@@ -3,6 +3,7 @@ package me.darkovrbaski.branchlibrary.config;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -12,10 +13,13 @@ import org.springframework.web.client.RestClient;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class HttpClientConfig {
 
+  @Value("${central-lib.host}")
+  String centralLibHost;
+
   @Bean
   public RestClient centralHttpClient() {
     return RestClient.builder()
-        .baseUrl("http://lib-cn:8080/central-library")
+        .baseUrl(String.format("http://%s:8080/central-library", centralLibHost))
         .build();
   }
 
